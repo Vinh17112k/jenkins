757fba3169d47cf8cc56bf681e7bbb3e39de7dbd
quangvinh <tranquangvinh17112k@gmail.com>
2023-05-23 14:55:55 +0700

    build jenkin CI/CD

---
+++ src/main/java/com/java/jenkin/controller/JenkinController.java
@@ -0,0 +1,14 @@
+package com.java.jenkin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jenkin")
+public class JenkinController {
+    @GetMapping("docker")
+    public String buildDocker(){
+        return "build docker CI/CD";
+    }
+}
